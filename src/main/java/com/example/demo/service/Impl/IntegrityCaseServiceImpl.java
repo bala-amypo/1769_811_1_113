5035973fd55a05791b1bd707beaa681aeb31382b
bala-amypo <bala@amypo.in>
2025-12-21 15:00:53 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/IntegrityCaseServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/IntegrityCaseServiceImpl.java
@@ -1,40 +1,41 @@
 package com.example.demo.service.impl;
 
-import java.util.List;
 import org.springframework.stereotype.Service;
+import jakarta.persistence.EntityNotFoundException;
 
-import com.example.demo.entity.IntegrityCase;
-import com.example.demo.repository.IntegrityCaseRepository;
+import com.example.demo.entity.*;
+import com.example.demo.repository.*;
 import com.example.demo.service.IntegrityCaseService;
+
+import java.util.List;
 
 @Service
 public class IntegrityCaseServiceImpl implements IntegrityCaseService {
 
     private final IntegrityCaseRepository repo;
+    private final StudentProfileRepository studentRepo;
 
-    public IntegrityCaseServiceImpl(IntegrityCaseRepository repo) {
+    public IntegrityCaseServiceImpl(
+            IntegrityCaseRepository repo,
+            StudentProfileRepository studentRepo) {
         this.repo = repo;
+        this.studentRepo = studentRepo;
     }
 
-    public IntegrityCase createCase(IntegrityCase c) {
+    public IntegrityCase createCase(Long studentId, IntegrityCase c) {
+
+        StudentProfile student = studentRepo.findById(studentId)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Student not found"));
+
+        c.setStudentProfile(student);
         return repo.save(c);
     }
 
-    public IntegrityCase updateCaseStatus(Long id, String status) {
-        IntegrityCase c = repo.findById(id).orElse(null);
-        if (c != null) {
-            c.setStatus(status);
-            return repo.save(c);
-        }
-        return null;
-    }
-
-    public List<IntegrityCase> getCasesByStudent(String studentId) {
-        return repo.findByStudentProfile_StudentId(studentId);
-    }
-
     public IntegrityCase getCaseById(Long id) {
-        return repo.findById(id).orElse(null);
+        return repo.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Case not found"));
     }
 
     public List<IntegrityCase> getAllCases() {
