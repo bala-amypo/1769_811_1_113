2189d17aaa096337143a376cb0e9b49483427cdd
bala-amypo <bala@amypo.in>
2025-12-21 06:33:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/RepeatOffenderRecordService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.entity.RepeatOffenderRecord;
+
+public interface RepeatOffenderRecordService {
+
+    RepeatOffenderRecord refreshRepeatOffenderData(String studentId);
+
+    RepeatOffenderRecord getRecordByStudent(String studentId);
+
+    List<RepeatOffenderRecord> getAllRepeatOffenders();
+}
