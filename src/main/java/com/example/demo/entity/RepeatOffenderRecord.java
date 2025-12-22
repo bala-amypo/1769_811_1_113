4f9e909ed3401af83d7da1455e2e2573f72d0838
bala-amypo <bala@amypo.in>
2025-12-22 07:34:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
+++ src/main/java/com/example/demo/entity/RepeatOffenderRecord.java
@@ -1,7 +1,7 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "repeat_offender_records")
@@ -18,43 +18,52 @@ private StudentProfile studentProfile;
 @Column(nullable = false)
 private Integer totalCases;
 
-private LocalDate lastIncidentDate;
+private LocalDate firstIncidentDate;
 
 @Column(nullable = false)
 private String flagSeverity;
 
-public RepeatOffenderRecord() {}
+public RepeatOffenderRecord() {
+}
 
-public RepeatOffenderRecord(StudentProfile studentProfile,
-Integer totalCases,
-LocalDate lastIncidentDate,
-String flagSeverity) {
-
+public RepeatOffenderRecord(StudentProfile studentProfile,Integer totalCases,String flagSeverity) {
 this.studentProfile = studentProfile;
 this.totalCases = totalCases;
-this.lastIncidentDate = lastIncidentDate;
 this.flagSeverity = flagSeverity;
 }
 
-public Long getId() { return id; }
-public void setId(Long id) { this.id = id; }
+public Long getId() {
+return id;
+}
 
-public StudentProfile getStudentProfile() { return studentProfile; }
+public StudentProfile getStudentProfile() {
+return studentProfile;
+}
+
 public void setStudentProfile(StudentProfile studentProfile) {
 this.studentProfile = studentProfile;
 }
 
-public Integer getTotalCases() { return totalCases; }
+public Integer getTotalCases() {
+return totalCases;
+}
+
 public void setTotalCases(Integer totalCases) {
 this.totalCases = totalCases;
 }
 
-public LocalDate getLastIncidentDate() { return lastIncidentDate; }
-public void setLastIncidentDate(LocalDate lastIncidentDate) {
-this.lastIncidentDate = lastIncidentDate;
+public LocalDate getFirstIncidentDate() {
+return firstIncidentDate;
 }
 
-public String getFlagSeverity() { return flagSeverity; }
+public void setFirstIncidentDate(LocalDate firstIncidentDate) {
+this.firstIncidentDate = firstIncidentDate;
+}
+
+public String getFlagSeverity() {
+return flagSeverity;
+}
+
 public void setFlagSeverity(String flagSeverity) {
 this.flagSeverity = flagSeverity;
 }
