7497e48887e32d29ec13043b739e1a5a34e8f632
bala-amypo <bala@amypo.in>
2025-12-24 05:28:50 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/AuthServiceImpl.java
@@ -1,7 +1,23 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.JwtResponse;
+import com.example.demo.security.JwtTokenProvider;
+import com.example.demo.service.AuthService;
+
 @Service
-public class AuthServiceImpl {
+public class AuthServiceImpl implements AuthService {
+
+private final JwtTokenProvider jwt;
+
+public AuthServiceImpl(JwtTokenProvider jwt) {
+this.jwt = jwt;
+}
+
+@Override
+public JwtResponse register(RegisterRequest request) {
+return new JwtResponse(jwt.generateToken(request.getEmail()), request.getRole());
+}
 }
