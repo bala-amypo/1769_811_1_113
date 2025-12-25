1707bf14a238c83b55db020e1dc44e68c1f3985f
bala-amypo <bala@amypo.in>
2025-12-25 05:35:57 +0000

    User commit

--- src/main/java/com/example/demo/servlet/BasicServlet.java
+++ src/main/java/com/example/demo/servlet/BasicServlet.java
@@ -1,6 +1,26 @@
 package com.example.demo.servlet;
 
+import java.io.IOException;
 
-public class BasicServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+public class BasicServlet extends HttpServlet {
+
+@Override
+protected void doGet(
+HttpServletRequest request,
+HttpServletResponse response
+) throws IOException {
+response.setStatus(HttpServletResponse.SC_OK);
+}
+
+@Override
+protected void doPost(
+HttpServletRequest request,
+HttpServletResponse response
+) throws IOException {
+response.setStatus(HttpServletResponse.SC_OK);
+}
 }
