b18b942f729cdf702629310f82de834444b1f97f
bala-amypo <bala@amypo.in>
2025-12-25 05:21:32 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
+import jakarta.servlet.http.HttpServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends HttpServlet {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(DemoApplication.class, args);
+}
 
 }
