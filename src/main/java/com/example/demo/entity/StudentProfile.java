b740c4198cac9cd6e9a9f68516ea85975beb0187
bala-amypo <bala@amypo.in>
2025-12-26 09:28:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -114,9 +114,7 @@ return createdAt;
 public List<IntegrityCase> getIntegrityCases() {
 return integrityCases;
 }
-public LocalDateTime getCreatedAt() {
-return createdAt;
-}
+
 
 public void setCreatedAt(LocalDateTime createdAt) {
 this.createdAt = createdAt;
