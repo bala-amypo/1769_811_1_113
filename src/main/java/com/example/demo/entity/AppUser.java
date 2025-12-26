60dbdf597ed8833132b94ce690b95e16bd381a25
bala-amypo <bala@amypo.in>
2025-12-26 09:48:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -1,20 +1,16 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "app_users",uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@Table(name = "app_users")
 public class AppUser {
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
-
-@Column(nullable = false)
-private String fullName;
 
 @Column(nullable = false, unique = true)
 private String email;
@@ -23,10 +19,7 @@ private String email;
 private String password;
 
 @Column(nullable = false)
-private Boolean enabled = true;
-
-@Column(nullable = false, updatable = false)
-private LocalDateTime createdAt;
+private String fullName;
 
 @ManyToMany(fetch = FetchType.EAGER)
 @JoinTable(
@@ -36,21 +29,10 @@ inverseJoinColumns = @JoinColumn(name = "role_id")
 )
 private Set<Role> roles = new HashSet<>();
 
-@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
 private Set<StudentProfile> studentProfiles = new HashSet<>();
 
-public AppUser() {}
-
-public AppUser(String fullName,String email,String password) {
-this.fullName = fullName;
-this.email = email;
-this.password = password;
-this.enabled = true;
-}
-
-@PrePersist
-protected void onCreate() {
-this.createdAt = LocalDateTime.now();
+public AppUser() {
 }
 
 public Long getId() {
@@ -59,15 +41,6 @@ return id;
 
 public void setId(Long id) {
 this.id = id;
-}
-
-
-public String getFullName() {
-return fullName;
-}
-
-public void setFullName(String fullName) {
-this.fullName = fullName;
 }
 
 public String getEmail() {
@@ -86,16 +59,12 @@ public void setPassword(String password) {
 this.password = password;
 }
 
-public Boolean getEnabled() {
-return enabled;
+public String getFullName() {
+return fullName;
 }
 
-public void setEnabled(Boolean enabled) {
-this.enabled = enabled;
-}
-
-public LocalDateTime getCreatedAt() {
-return createdAt;
+public void setFullName(String fullName) {
+this.fullName = fullName;
 }
 
 public Set<Role> getRoles() {
@@ -113,5 +82,4 @@ return studentProfiles;
 public void setStudentProfiles(Set<StudentProfile> studentProfiles) {
 this.studentProfiles = studentProfiles;
 }
-
 }
