fe20e31144a65926343e8070f9b0d8afc4f6ef83
bala-amypo <bala@amypo.in>
2025-12-27 04:58:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -54,12 +54,7 @@ public StudentProfile() {
 this.createdAt = LocalDateTime.now();
 }
 
-@PrePersist
-protected void onCreate() {
-if (this.createdAt == null) {
-this.createdAt = LocalDateTime.now();
-}
-}
+
 
 /* getters & setters unchanged */
 
