aebe4795202391c5fd6bc84f41ed56e0af408478
bala-amypo <bala@amypo.in>
2025-12-28 18:00:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -23,6 +23,7 @@ private String password;
 @Column(name = "full_name", nullable = false)
 private String fullName;
 
+private String name;
 @Column(nullable = false)
 private boolean enabled = true;
 
@@ -49,7 +50,8 @@ public void setId(Long id) { this.id = id; }
 
 public String getEmail() { return email; }
 public void setEmail(String email) { this.email = email; }
-
+public String getName() { return name; }
+public void setName(String name) { this.name = name; }
 public String getPassword() { return password; }
 public void setPassword(String password) { this.password = password; }
 
