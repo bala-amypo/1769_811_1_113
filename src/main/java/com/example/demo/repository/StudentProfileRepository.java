1162c361275e445f1009f4bf795b0f7b88fb5771
bala-amypo <bala@amypo.in>
2025-12-30 02:37:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentProfileRepository.java
+++ src/main/java/com/example/demo/repository/StudentProfileRepository.java
@@ -1,10 +1,24 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.StudentProfile;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.StudentProfile;
 
 @Repository
 public interface StudentProfileRepository
 extends JpaRepository<StudentProfile, Long> {
+
+/* Find student using business identifier */
+Optional<StudentProfile> findByStudentId(String studentId);
+
+/* Find student using email */
+Optional<StudentProfile> findByEmail(String email);
+
+/* Check existence (used during registration) */
+boolean existsByStudentId(String studentId);
+
+boolean existsByEmail(String email);
 }
